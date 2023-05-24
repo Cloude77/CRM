6ba826ec85dabe71fd89bc139de5bd9359895480
Sergio <neftianik@inbox.ru>
2023-05-24 11:55:56 +0300

    Create templates: navbar, base, home

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '1',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
